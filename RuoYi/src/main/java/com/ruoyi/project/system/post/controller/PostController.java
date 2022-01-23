0f43b0c2b6e86588778bab54d85b6cb14c57ad92
gao-gam <wll09837347@163.com>
2022-01-23 19:16:20 +0800

    RuoYi

--- RuoYi/src/main/java/com/ruoyi/project/system/post/controller/PostController.java
+++ RuoYi/src/main/java/com/ruoyi/project/system/post/controller/PostController.java
@@ -45,6 +45,7 @@ public class PostController extends BaseController
     @ResponseBody
     public TableDataInfo list(Post post)
     {	//这是gao-gam的代码
+		//邮箱是wll09837347@163.com
         startPage();
         List<Post> list = postService.selectPostList(post);
         return getDataTable(list);
