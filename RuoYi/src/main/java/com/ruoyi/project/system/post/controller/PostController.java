ff3dc4ce552b849c85d35827aaabd80a2b513ff0
Administrator <Administrator@PC-20220222GMAZ>
2022-07-26 20:11:39 +0800

    gao-gam

--- RuoYi/src/main/java/com/ruoyi/project/system/post/controller/PostController.java
+++ RuoYi/src/main/java/com/ruoyi/project/system/post/controller/PostController.java
@@ -49,7 +49,7 @@ public class PostController extends BaseController
 		//这是sxt的数据
 		//这是gao-gam更改的
 		//这是sxt更改的
-    	//这是post列表;
+    	//这是post列表--
         startPage();
 		List<Post> list = postService.selectPostList(post);
         return getDataTable(list);
